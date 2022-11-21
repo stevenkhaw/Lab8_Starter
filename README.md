e56eea8833ae0188a21650d98043992efa4d91f7
Steven Khaw <swkhaw8226@yahoo.com>
2022-11-20 18:44:08 -0800

    complete

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # Lab 8 - Starter
+
+I worked alone.
+
+1. You would fit automated tests within a Github action that runs whenever code is pushed. While this may seem unintuiative early on, this will help catch bugs early on and ensuring that code created is to expected quality as well as meeting its requirements.
+
+2. No.
+
+3. No. This would require quite a few moving parts and would most likely be too complicated to fit inside one unit test. Unit tests are supposed to be quick and do not test how individual components interact with each other on a feature level.
+
+4. Yes. This is a relatively simple test that doesn't go out of bounds and does not have any large-scale moving parts. This can easily be tested as well.
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,92 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+
+// tests isPhoneNumber()
+test('checks (626)-825-5186 to equal true', () => {
+    expect(functions.isPhoneNumber('(626)-825-5186')).toBe(true);
+});
+
+test('checks 825-5186 to equal true', () => {
+    expect(functions.isPhoneNumber('825-5186')).toBe(true);
+});
+
+test('checks 1122334455667788 to equal false', () => {
+    expect(functions.isPhoneNumber('1122334455667788')).toBe(false);
+});
+
+test('checks 123 to equal false', () => {
+    expect(functions.isPhoneNumber('123')).toBe(false);
+});
+
+
+// tests isEmail()
+test('checks skhaw@ucsd.edu to equal true', () => {
+    expect(functions.isEmail('skhaw@ucsd.edu')).toBe(true);
+});
+
+test('checks thisisareallylongemail@thisisareallylongemail.com to equal true', () => {
+    expect(functions.isEmail('thisisareallylongemail@thisisareallylongemail.com')).toBe(true);
+});
+
+test('checks skhaw@1111.edu to equal false', () => {
+    expect(functions.isEmail('skhaw@1111.edu')).toBe(false);
+});
+
+test('checks thisisareallylongemail@thisisareallylongemail.thisisareallylongemail to equal false', () => {
+    expect(functions.isEmail('thisisareallylongemail@thisisareallylongemail.thisisareallylongemail')).toBe(false);
+});
+
+
+// tests isStrongPassword()
+test('checks four to equal true', () => {
+    expect(functions.isStrongPassword('four')).toBe(true);
+});
+
+test('checks Strong12_ to equal true', () => {
+    expect(functions.isStrongPassword('Strong12_')).toBe(true);
+});
+
+test('checks Strong12** to equal false', () => {
+    expect(functions.isStrongPassword('Strong12**')).toBe(false);
+});
+
+test('checks thisisareallylongpasswordtotestbrobuthelpmeplease to equal false', () => {
+    expect(functions.isStrongPassword('thisisareallylongpasswordtotestbrobuthelpmeplease')).toBe(false);
+});
+
+
+// tests isDate()
+test('checks 1/22/3344 to equal true', () => {
+    expect(functions.isDate('1/22/3344')).toBe(true);
+});
+
+test('checks 11/22/3344 to equal true', () => {
+    expect(functions.isDate('11/22/3344')).toBe(true);
+});
+
+test('checks 11/22/334 to equal false', () => {
+    expect(functions.isDate('11/22/334')).toBe(false);
+});
+
+test('checks 111/22/3344 to equal false', () => {
+    expect(functions.isDate('111/22/3344')).toBe(false);
+});
+
+
+// tests isHexColor() 
+test('checks #FF5733 to equal true', () => {
+    expect(functions.isHexColor('#FF5733')).toBe(true);
+});
+
+test('checks #fff to equal true', () => {
+    expect(functions.isHexColor('#fff')).toBe(true);
+});
+
+test('checks #ff00002 to equal false', () => {
+    expect(functions.isHexColor('#ff00002')).toBe(false);
+});
+
+test('checks #1 to equal false', () => {
+    expect(functions.isHexColor('#1')).toBe(false);
+});
